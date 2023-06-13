1996edaae558525e0e18834c765727dccab10185
sravan <sravan-s@protonmail.com>
2023-06-14 01:21:59 +0900

    feat: Traverse and list commits

--- change_log/src/main.rs
+++ change_log/src/main.rs
@@ -1,25 +1,37 @@
 use git2::{Commit, Repository};
+use std::error;
+use std::path::PathBuf;
+
 // need to keep a list of-
 // commitId, commitMessage
 
 // there are 3 types of commits-
-// feature, bugfix,
+// feature, bugfix, chore(others)
+// go through each commit ->
+// * pick p
+// * drop d
+// * stop s
+// * cancel c
 
 // cargo watch -x run
+fn print_commits(path: PathBuf) -> Result<i32, Box<dyn error::Error>> {
+    let repo = Repository::discover(path)?;
+    let mut revwalk = repo.revwalk()?;
+    revwalk.push_head()?;
+    revwalk.set_sorting(git2::Sort::TIME)?;
+    for rev in revwalk {
+        let commit = repo.find_commit(rev?)?;
+        let message = commit
+            .summary_bytes()
+            .unwrap_or_else(|| commit.message_bytes());
+        println!("{}\t{}", commit.id(), String::from_utf8_lossy(message));
+    }
+    Ok(0)
+}
+
 fn main() {
     let _commits: Vec<Commit> = Vec::new();
-    loop {
-        let repo: Repository = match Repository::open("../") {
-            Ok(repo) => repo,
-            Err(e) => panic!("Failed to open a repo here: {}", e),
-        };
-
-        let origin = match repo.find_remote("origin") {
-            Ok(r) => r,
-            Err(e) => panic!("No remotes{}", e),
-        };
-
-        println!("origin is {}", origin.url().unwrap());
-        panic!("Exit");
-    }
+    let mut path = PathBuf::new();
+    path.push("../");
+    let _ = print_commits(path);
 }
