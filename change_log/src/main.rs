cfd64dfa72b3bdb667cd0a00d3bc70d901933077
sravan <sravan-s@protonmail.com>
2023-06-21 09:33:32 +0900

    fix: borrowing on fns

--- change_log/src/main.rs
+++ change_log/src/main.rs
@@ -1,6 +1,5 @@
 use git2::{Commit, Repository};
-use std::error;
-use std::path::PathBuf;
+use std::{error::Error, io::stdin, path::PathBuf};
 
 // need to keep a list of-
 // commitId, commitMessage
@@ -12,10 +11,16 @@ use std::path::PathBuf;
 // * drop d
 // * stop s
 // * cancel c
+enum KeyPress {
+    P,
+    D,
+    S,
+    C,
+}
 
 // cargo watch -x run
-fn print_commits(path: PathBuf) -> Result<i32, Box<dyn error::Error>> {
-    let repo = Repository::discover(path)?;
+fn get_commits(repo: &Repository) -> Result<Vec<Commit>, Box<dyn Error>> {
+    let mut static_commits: Vec<Commit> = Vec::new();
     let mut revwalk = repo.revwalk()?;
     revwalk.push_head()?;
     revwalk.set_sorting(git2::Sort::TIME)?;
@@ -25,13 +30,44 @@ fn print_commits(path: PathBuf) -> Result<i32, Box<dyn error::Error>> {
             .summary_bytes()
             .unwrap_or_else(|| commit.message_bytes());
         println!("{}\t{}", commit.id(), String::from_utf8_lossy(message));
+        println!("Press: p to Pick, d to Drop, s to Stop, c to Cancel, then press Enter");
+        let mut input = String::new();
+        stdin().read_line(&mut input)?;
+        let i = input.chars().last().unwrap_or_default();
+        if i == 'p' {
+            static_commits.push(commit.clone());
+        }
+        if i == 'd' {
+            continue;
+        }
+        if i == 's' {
+            break;
+        }
+        if i == 'c' {
+            panic!("terminating the program")
+        }
     }
-    Ok(0)
+    return Ok(static_commits);
 }
 
-fn main() {
-    let _commits: Vec<Commit> = Vec::new();
+fn get_repo(path: PathBuf) -> Result<Repository, Box<dyn Error>> {
+    let repo: Repository = Repository::discover(&path)?;
+    Ok(repo)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let mut path = PathBuf::new();
-    path.push("../");
-    let _ = print_commits(path);
+    println!("Input relative repo path");
+    let mut repo_path = String::new();
+    stdin().read_line(&mut repo_path)?;
+    path.push(repo_path);
+
+    let repo = get_repo(path)?;
+    let commits = get_commits(&repo)?;
+    println!(
+        "repo is worktree-> {}; commits len {}",
+        repo.is_worktree(),
+        commits.len()
+    );
+    Ok(())
 }
