7a564f8cb425411e3fb3484c230e34ed707df445
sravan <sravan-s@protonmail.com>
2023-06-22 23:21:47 +0900

    fix: Format of reading keypress

--- change_log/src/main.rs
+++ change_log/src/main.rs
@@ -1,5 +1,9 @@
 use git2::{Commit, Repository};
-use std::{error::Error, io::stdin, path::PathBuf};
+use std::{
+    error::Error,
+    io::{stdin, Read},
+    path::PathBuf,
+};
 
 // need to keep a list of-
 // commitId, commitMessage
@@ -20,22 +24,35 @@ enum KeyPress {
 
 // cargo watch -x run
 fn get_commits(repo: &Repository) -> Result<Vec<Commit>, Box<dyn Error>> {
+    // initialize
     let mut static_commits: Vec<Commit> = Vec::new();
     let mut revwalk = repo.revwalk()?;
     revwalk.push_head()?;
     revwalk.set_sorting(git2::Sort::TIME)?;
+
+    // start walking through commits
     for rev in revwalk {
         let commit = repo.find_commit(rev?)?;
         let message = commit
             .summary_bytes()
             .unwrap_or_else(|| commit.message_bytes());
+
+        // Show commit & read input
         println!("{}\t{}", commit.id(), String::from_utf8_lossy(message));
-        println!("Press: p to Pick, d to Drop, s to Stop, c to Cancel, then press Enter");
-        let mut input = String::new();
-        stdin().read_line(&mut input)?;
-        let i = input.chars().last().unwrap_or_default();
+        println!("Press: p to Pick, d to Drop, s to Stop(or finish), c to Cancel");
+        let mut buffer = [0u8; 1];
+        let i: char = match stdin().read_exact(&mut buffer) {
+            Ok(_) => buffer[0] as char,
+            Err(e) => {
+                println!("Error reading input{}", e);
+                '\0'
+            }
+        };
+
+        // Let user select what to do with input
         if i == 'p' {
             static_commits.push(commit.clone());
+            continue;
         }
         if i == 'd' {
             continue;
@@ -46,6 +63,7 @@ fn get_commits(repo: &Repository) -> Result<Vec<Commit>, Box<dyn Error>> {
         if i == 'c' {
             panic!("terminating the program")
         }
+        println!("Unknown input");
     }
     return Ok(static_commits);
 }
@@ -60,6 +78,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Input relative repo path");
     let mut repo_path = String::new();
     stdin().read_line(&mut repo_path)?;
+
+    let last_char = repo_path.chars().last().unwrap();
+    if last_char == '\n' {
+        repo_path.pop();
+    }
     path.push(repo_path);
 
     let repo = get_repo(path)?;
